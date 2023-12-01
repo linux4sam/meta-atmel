linux4sam/meta-atmel

commit 6261dc7956a8a5dfe844a07d117b0b619906ed59

recipes-core:systemd: add support to enable network connectivity
Append systemd-conf changes to include network template configuration
files to enable network connectivity in systemd based init system.

Signed-off-by: Balakrishnan Sambath <balakrishnan.s@microchip.com>
Acked-by: Hari Prasath Gujulan Elango <Hari.PrasathGE@microchip.com>

recipes-core/systemd/systemd-conf_%.bbappend
@@ -1,0 +1,17 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
+
+SRC_URI += " \
+    file://eth.network \
+     file://wlan.network \
+"
+
+FILES:${PN} += " \
+    ${sysconfdir}/systemd/network/eth.network \
+     ${sysconfdir}/systemd/network/wlan.network \
+"
+
+do_install () {
+    install -d ${D}${sysconfdir}/systemd/network
+    install -m 0644 ${WORKDIR}/eth.network ${D}${sysconfdir}/systemd/network
+     install -m 0644 ${WORKDIR}/wlan.network ${D}${sysconfdir}/systemd/network
+}

recipes-core/systemd/files/eth.network
@@ -1,0 +1,9 @@
+[Match]
+Name=eth*
+
+[Network]
+DHCP=yes
+#Address=192.168.7.2
+#Netmask=255.255.255.0
+#Network=192.168.7.0
+#Gateway=192.168.7.1

recipes-core/systemd/files/wlan.network
@@ -1,0 +1,9 @@
+[Match]
+Name=wlan*
+
+[Network]
+DHCP=yes
+#Address=192.168.8.2
+#Netmask=255.255.255.0
+#Network=192.168.8.0
+#Gateway=192.168.8.1

commit 123b094a13b637f647dd16deedaa823f9d537dce

recipes-core: systemd: Fix alignment and overriding issue
Fix the alignment issue and add append in do_install(), do not override
the function.

Fixes: 6261dc7 ("recipes-core:systemd: add support to enable network connectivity")
Signed-off-by: Dharma Balasubiramani <dharma.b@microchip.com>
Signed-off-by: Hari Prasath Gujulan Elango <Hari.PrasathGE@microchip.com>

recipes-core/systemd/systemd-conf_%.bbappend
@@ -1,17 +1,16 @@
-FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
-SRC_URI += " \
+SRC_URI:append = " \
     file://eth.network \
-     file://wlan.network \
+    file://wlan.network \
 "
 
 FILES:${PN} += " \
-    ${sysconfdir}/systemd/network/eth.network \
-     ${sysconfdir}/systemd/network/wlan.network \
+    ${sysconfdir}/systemd/network/ \
 "
 
-do_install () {
+do_install:append () {
     install -d ${D}${sysconfdir}/systemd/network
     install -m 0644 ${WORKDIR}/eth.network ${D}${sysconfdir}/systemd/network
-     install -m 0644 ${WORKDIR}/wlan.network ${D}${sysconfdir}/systemd/network
+    install -m 0644 ${WORKDIR}/wlan.network ${D}${sysconfdir}/systemd/network
 }