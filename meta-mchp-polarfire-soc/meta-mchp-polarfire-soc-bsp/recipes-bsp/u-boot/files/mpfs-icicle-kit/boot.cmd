f40e626bce309eb6efe4e0cab745260b955fa3d4
Valentina Fernandez <valentina.fernandezalanis@microchip.com>
2025-07-07 11:11:30 +0100

    meta-mchp-polarfire-soc: recipes-bsp: apply CPU opp overlay in icicle kit
    Update the Icicle Kit U-Boot boot script to apply the CPU operating
    point overlay, enabling adjustment of the CPU frequency in Linux based on
    predefined operating points.
    
    Signed-off-by: Valentina Fernandez <valentina.fernandezalanis@microchip.com>

--- meta-mchp-polarfire-soc/meta-mchp-polarfire-soc-bsp/recipes-bsp/u-boot/files/mpfs-icicle-kit/boot.cmd
+++ meta-mchp-polarfire-soc/meta-mchp-polarfire-soc-bsp/recipes-bsp/u-boot/files/mpfs-icicle-kit/boot.cmd
@@ -5,7 +5,7 @@ fdt addr ${fdtcontroladdr}
 fdt get value board_compatible / compatible 1
 setenv fitconf conf-${board_compatible}.dtb
 load mmc 0:${distro_bootpart} ${scriptaddr} fitImage
-bootm start ${scriptaddr}#${fitconf}
+bootm start ${scriptaddr}#${fitconf}#conf-microchip,mpfs_icicle_cpu_opp.dtbo
 bootm loados ${scriptaddr};
 # Try to load a ramdisk if available inside fitImage
 bootm ramdisk;
